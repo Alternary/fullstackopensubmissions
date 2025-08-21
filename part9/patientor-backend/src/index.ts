bb2c9d3bc5473c63fc065e8bbd64101577129f07
alternary <aapelisyren@proton.me>
2025-08-21 17:26:17 +0300

    9.14 tehty

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 
+import { z } from 'zod';
+
 import { v1 as uuid } from 'uuid';
 
 const app = express();
@@ -53,34 +55,35 @@ app.get('/api/patients', (_req,res) => {
   res.send(ps);
 });
 
-const isString = (text: unknown): text is string => {
-  return typeof text === 'string' || text instanceof String;
-};
+// const isString = (text: unknown): text is string => {
+//   return typeof text === 'string' || text instanceof String;
+// };
 
-const parseName = (name: unknown): string => {
-  if (!isString(name)) {
-    throw new Error('Incorrect or missing name');
-  }
-  return name;
-};
+// const parseName = (name: unknown): string => {
+//   // if (!isString(name)) {
+//   //   throw new Error('Incorrect or missing name');
+//   // }
+//   // return name;
+//   return z.string().parse(name);
+// };
 
-const isDate = (date: string): boolean => {
-  return Boolean(Date.parse(date));
-};
+// const isDate = (date: string): boolean => {
+//   return Boolean(Date.parse(date));
+// };
+//
+// const parseDateOfBirth = (dateOfBirth: unknown): string => {
+//   if (!isString(dateOfBirth) || !isDate(dateOfBirth)) {
+//       throw new Error('Incorrect or missing date: ' + dateOfBirth);
+//   }
+//   return dateOfBirth;
+// };
 
-const parseDateOfBirth = (dateOfBirth: unknown): string => {
-  if (!isString(dateOfBirth) || !isDate(dateOfBirth)) {
-      throw new Error('Incorrect or missing date: ' + dateOfBirth);
-  }
-  return dateOfBirth;
-};
-
-const parseSsn = (ssn: unknown): string => {
-  if (!isString(ssn)) {
-    throw new Error('Incorrect or missing ssn: ' + ssn);
-  }
-  return ssn;
-};
+// const parseSsn = (ssn: unknown): string => {
+//   if (!isString(ssn)) {
+//     throw new Error('Incorrect or missing ssn: ' + ssn);
+//   }
+//   return ssn;
+// };
 
 enum Gender {
   Male = 'male',
@@ -88,23 +91,23 @@ enum Gender {
   Other = 'other'
 }
 
-const isGender = (param: string): param is Gender => {
-  return Object.values(Gender).map(v => v.toString()).includes(param);
-};
+// const isGender = (param: string): param is Gender => {
+//   return Object.values(Gender).map(v => v.toString()).includes(param);
+// };
+//
+// const parseGender = (gender: unknown): string => {
+//   if (!isString(gender) || !isGender(gender)) {
+//     throw new Error('Incorrect or missing gender: ' + gender);
+//   }
+//   return gender;
+// };
 
-const parseGender = (gender: unknown): string => {
-  if (!isString(gender) || !isGender(gender)) {
-    throw new Error('Incorrect or missing gender: ' + gender);
-  }
-  return gender;
-};
-
-const parseOccupation = (occupation: unknown): string => {
-  if (!isString(occupation)) {
-    throw new Error('Incorrect or missing occupation: ' + occupation);
-  }
-  return occupation;
-};
+// const parseOccupation = (occupation: unknown): string => {
+//   if (!isString(occupation)) {
+//     throw new Error('Incorrect or missing occupation: ' + occupation);
+//   }
+//   return occupation;
+// };
 
 const toNewPatient = (object : unknown) : Patient => {
   if (!object || typeof object !== 'object') {
@@ -113,11 +116,11 @@ const toNewPatient = (object : unknown) : Patient => {
   if ('name' in object && 'dateOfBirth' in object && 'ssn' in object && 'gender' in object && 'occupation' in object) {
     const newPatient = {
       id: uuid(),
-      name: parseName(object.name),
-      dateOfBirth: parseDateOfBirth(object.dateOfBirth),
-      ssn: parseSsn(object.ssn),
-      gender: parseGender(object.gender),
-      occupation: parseOccupation(object.occupation)
+      name: z.string().parse(object.name),
+      dateOfBirth: z.string().date().parse(object.dateOfBirth),
+      ssn: z.string().parse(object.ssn),
+      gender: z.enum(Gender).parse(object.gender),
+      occupation: z.string().parse(object.occupation)
     };
     return newPatient;
   }
